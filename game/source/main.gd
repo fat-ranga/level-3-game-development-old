302e06a1f5bf1107111a744b1ff71a0858628cda
Kael Robertson <robertsonk@mbas.ac.nz>
2023-03-22 12:05:35 +1300

    Start work on main menu

--- game/source/main.gd
+++ game/source/main.gd
@@ -5,13 +5,18 @@ extends Node
 
 var current_status: String = ""
 
+@onready var main_menu_scene = "res://scenes/ui/main_menu.tscn" # TODO: figure out type, not PackedScene?
+
 @onready var ROOT_DIRECTORY: String = str(OS.get_executable_path().get_base_dir())
 @onready var BLOCK_TYPES_PATH: String = str(ROOT_DIRECTORY + "/block_types.json")#"user://textures"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Load all necessary resources when the game starts.
-	load_resources()
+	#load_resources()
+	
+	# Open up the main menu.
+	open_main_menu()
 
 func load_resources() -> void:
 	current_status = "Generating texture atlas..."
@@ -21,6 +26,10 @@ func load_resources() -> void:
 	
 	current_status = "Reading block types..."
 	data_importer.get_json_data(BLOCK_TYPES_PATH)
+
+func open_main_menu() -> void:
+	
+	call_deferred("add_child", main_menu_scene)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
